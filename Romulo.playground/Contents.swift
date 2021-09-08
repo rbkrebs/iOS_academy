89b9d9dceb1cfc1209307e94be2fd83b0fb1e065
rbkrebs <romulo.krebs@gmail.com>
2021-09-08 15:52:31 -0300

    first commit

---
+++ Romulo.playground/Contents.swift
@@ -0,0 +1,15 @@
+import UIKit
+
+
+func returnIfAllLetterAreEqual(word : String) -> Bool{
+    
+    
+    return true
+}
+
+//Write a function that returns true if all letters are different in a given string
+
+
+var result = returnIfAllLetterAreEqual(word: "oi")
+
+print(result)
